ac43c384bedd5c975d1f163ba64a683d9318bbda
FirdeusGj <firdegjepali@gmail.com>
2023-05-20 22:19:23 +0200

    finished most of it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
@@ -12,6 +13,7 @@ function App() {
       <Reasons/>
       <Plans/>
       <Testimonials/>
+      <Footer/>
     </div>
   );
 }
