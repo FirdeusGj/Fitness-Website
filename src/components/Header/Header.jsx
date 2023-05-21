6eab2cca4b49bab1482757f3ae7c20925592c7fb
FirdeusGj <firdegjepali@gmail.com>
2023-05-21 23:21:27 +0200

    I think im finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
-import './Header.css'
-import Logo from '../../assets/logo.png'
-
+import React from "react";
+import "./Header.css";
+import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+import { useState } from "react";
+import { Link } from "react-scroll";
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
     <div className="header">
-        <img src={Logo} alt="" className='logo' />
-        <ul className='header-menu'>
-            <li>Home</li>
-            <li>Programs</li>
-            <li>Why Us</li>
-            <li>Plans</li>
-            <li>Testimonials</li>
+      <img src={Logo} alt="" className="logo" />
+      {menuOpened === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img src={Bars} alt="" style={{ width: "1.5rem", heigh: "1.5rem" }} />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li>
+            <Link smooth={true} onClick={() => setMenuOpened(false)} to="header">Home</Link>
+          </li>
+          <li>
+            <Link smooth={true} onClick={() => setMenuOpened(false)} to="programs">Programs</Link>
+          </li>
+          <li>
+            <Link smooth={true} onClick={() => setMenuOpened(false)} to='reasons'>Why Us</Link>
+          </li>
+          <li>
+            <Link smooth={true} onClick={() => setMenuOpened(false)} to="plans">Plans</Link>
+          </li>
+          <li>
+            <Link smooth={true} onClick={() => setMenuOpened(false)} to="Testimonials">
+              Testimonials
+            </Link>
+          </li>
         </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,10 +5,12 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
+import NumberCounter from 'number-counter'
 
 import { motion } from "framer-motion";
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerWidth<=768 ? true: false;
   return (
     <div className="hero">
       <div className="blur hero-blur"></div>
@@ -17,7 +19,7 @@ const Hero = () => {
         {/* the best ad */}
         <div className="the-best-ad">
           <motion.div
-            initial={{ left: "238px" }}
+            initial={{ left: mobile? '165px' : '238px' }}
             whileInView={{ left: "8px" }}
             transition={{ ...transition, type: "tween" }}
           ></motion.div>
@@ -40,15 +42,15 @@ const Hero = () => {
         {/* Figures */}
         <div className="figures">
           <div>
-            <span>+140</span>
+            <span><NumberCounter end={140} start={50} delay="4" preFix='+'/></span>
             <span>Expert coaches</span>
           </div>
           <div>
-            <span>+978</span>
+            <span><NumberCounter end={978} start={700} delay="4" preFix='+'/></span>
             <span>members joined</span>
           </div>
           <div>
-            <span>+50</span>
+            <span><NumberCounter end={50} start={0} delay="4" preFix='+'/></span>
             <span>fitness programs</span>
           </div>
         </div>
